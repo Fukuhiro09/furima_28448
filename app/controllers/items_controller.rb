d266a329a7a6d093a2a8933027d3e2c7181cca4a
Fukuhiro09 <68676051+Fukuhiro09@users.noreply.github.com>
2020-09-08 14:23:25 +0900

    商品情報編集の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -20,6 +20,16 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @tweet = Item.find(params[:id])
+  end
+
+  def update
+    item = Item.find(params[:id])
+    item.update(item_params)
+  end
+
+
 
   private
 
