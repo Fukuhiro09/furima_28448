f3fcfbcacd0330977522600cd65209000d187571
Fukuhiro09 <68676051+Fukuhiro09@users.noreply.github.com>
2020-09-24 15:01:26 +0900

    テストコードの修正

--- spec/models/user_purchase_spec.rb
+++ spec/models/user_purchase_spec.rb
@@ -8,11 +8,16 @@ RSpec.describe UserPurchase, type: :model do
     end
 
     context '購入がうまくいくとき' do
-      it "postal_codeとcityとhouse_numberとphone_numberとprefecture_idが存在すれば登録できる"
+      it "card_tokenとpostal_codeとcityとhouse_numberとphone_numberとprefecture_idが存在すれば登録できる"
       expect(@user_purchase).to be_valid
     end
 
     context '購入がうまくいかないとき' do
+      it "card_tokenが空だと登録できない"
+        @user_purchase.card_token = ""  
+        @user_purchase.valid?
+        expect(@user_purchase.errors.full_messages).to include("Card token can't be blank ")
+      end
       it "postal_codeが空だと登録できない" do
         @user_purchase.postal_code = ""  
         @user_purchase.valid?
@@ -37,6 +42,16 @@ RSpec.describe UserPurchase, type: :model do
         @user_purchase.prefecture_id = ""  
         @user_purchase.valid?
         expect(@user_purchase.errors.full_messages).to include("Prefecture can't be blank")
+      end
+      it "postal_codeにハイフンがないと登録できない"
+        @user_purchase.postal_code = "1234444"  
+        @user_purchase.valid?
+        expect(@user_purchase.errors.full_messages).to include("Postal code 空では登録できません")
+      end
+      it "phone_numberがハイフンなしの11桁以内出ないと登録できません" do
+        @user_purchase.phone_number = "090-1234-5678"
+        @user_purchase.valid?
+        expect(@user_purchase.errors.full_messages).to include("Phone numberハイフンなしの11桁以内出ないと登録できません")
       end
     end
   end
