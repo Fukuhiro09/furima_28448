507b4237ede8093981b81b82e0275bb548b6bea5
Fukuhiro09 <68676051+Fukuhiro09@users.noreply.github.com>
2020-09-25 15:56:52 +0900

    Userテストコードの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,9 @@
 FactoryBot.define do
   # 出品者用データ factory名は「seller」とします
-  factory :seller, class: User do
-    sequence(:nickname)        { |i| "出品者_#{i}"}
-    sequence(:email)           { |i| "seller_#{i}@test.com"}
+  factory :user do
     
-    password              { '00000000' }
-    password_confirmation { password }
-    birthday              { Date.today }
-    first_name            { '鈴木' }
-    last_name             { '隆' }
-    kana_first_name       { 'スズキ' }
-    kana_last_name        { 'タカシ' }
-  end
-
- # 購入者用データ factory名は「buyer」とします。
-  factory :buyer, class: User do
-    sequence(:nickname)        { |i| "出品者_#{i}"}
-    sequence(:email)           { |i| "seller_#{i}@test.com"}
-    
+    nickname              { '出品者' }
+    email                 { 'test@test.com' }
     password              { '00000000' }
     password_confirmation { password }
     birthday              { Date.today }
