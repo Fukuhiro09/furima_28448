d0eaa6b38b2086957b47580d8a2a348bce864fe8
Fukuhiro09 <68676051+Fukuhiro09@users.noreply.github.com>
2020-09-25 16:16:42 +0900

    itemテストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,18 +1,15 @@
 FactoryBot.define do
-  factory :seller_item, class: Item do
-    sequence(:product_name) { |i| "product_#{i}"}
-    sequence(:info) { |i| "info_#{i}"}
+  factory :item do
 
-    
+
+    product_name          { '果物' }
+    info                  { '新鮮' }
     category_id           { 'メンズ' }
     sales_status_id       { '未使用に近い' }
     shipping_fee_id       { '着払い' }
     prefecture_id         { '北海道' }
     scheduled_delivery_id { '1〜2日で発送' }
     price                 { 400 }
-      #出品者のaddressと連携します。
-      association :user_address, factory: :seller_address
-      #出品者のaddress内にあるuserと連携します。
-      user             {user_address.user}
+     
   end
 end
